92d16fd59937b2127c6101b15ed38a97c6f4e58d
Demianight <komarovdemian@yandex.ru>
2025-06-07 22:35:49 +0300

    Fixed pathing

--- messages/routers.py
+++ messages/routers.py
@@ -27,14 +27,6 @@ def create_msg(
     return create_message(session, msg)
 
 
-@router.get("/{message_id}", response_model=Message)
-def get_msg(message_id: int, session: Session = Depends(get_session)):
-    msg = get_message_by_id(session, message_id)
-    if not msg:
-        raise HTTPException(status_code=404, detail="Message not found")
-    return msg
-
-
 @router.get("/unprocessed", response_model=list[Message])
 def get_unproc(session: Session = Depends(get_session)):
     return get_unprocessed_messages(session)
@@ -43,3 +35,11 @@ def get_unproc(session: Session = Depends(get_session)):
 @router.post("/{message_id}/process", response_model=Message)
 def process(message_id: int, session: Session = Depends(get_session)):
     return process_message(session, message_id)
+
+
+@router.get("/{message_id}", response_model=Message)
+def get_msg(message_id: int, session: Session = Depends(get_session)):
+    msg = get_message_by_id(session, message_id)
+    if not msg:
+        raise HTTPException(status_code=404, detail="Message not found")
+    return msg
